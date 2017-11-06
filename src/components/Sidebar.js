9ea857b905134d352a0d002f6905b563bf2361b6
reactionic127 <richard.gill127@gmail.com>
2017-11-06 08:47:53 +0100

    added icon group

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
+import {Image} from 'react-bootstrap';
 import '../styles/App.css';
+import defaultUser from '../assets/default.png'
 
 class Sidebar extends Component {
 	constructor(props) {
@@ -17,7 +19,22 @@ class Sidebar extends Component {
     render() {
 		return (
             <div className="sidebar-container">
-                
+                <div className="app-logo">
+                    <span className="logo-text">
+                        audienti
+                    </span>
+                </div>
+                <div className="user-info">
+                    <Image src={defaultUser} className="userIcon" />
+                    <span className="user-email">
+                        wflanagan@audienti.com
+                    </span>
+                    <div className="icon-group">
+                        <i className="fa fa-cog fa-lg custom-icon" aria-hidden="true"></i>
+                        <i className="fa fa-building fa-lg custom-icon margin-l-20" aria-hidden="true"></i>
+                        <i className="fa fa-commenting fa-lg custom-icon margin-l-20" aria-hidden="true"></i>
+                    </div>
+                </div>
             </div>
 		);
 	}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import './styles/index.css';
 import App from './containers/App';
 import registerServiceWorker from "./registerServiceWorker";
+import './styles/index.css';
+import 'font-awesome/css/font-awesome.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();

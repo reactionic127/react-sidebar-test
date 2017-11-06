84655223640eb912cdad94d33467c14c890195b3
reactionic127 <richard.gill127@gmail.com>
2017-11-06 15:27:48 +0100

    breadcrumb menu is implemented

--- src/containers/Projects.js
+++ src/containers/Projects.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
-import { Breadcrumb } from 'react-bootstrap'
+import { Col, Breadcrumb } from 'react-bootstrap'
 import Sidebar from '../components/Sidebar';
-import { BreadcrumbsWithIcon } from '../components/AppBreadcrumbs';
 import '../styles/App.css';
 
 class Projects extends Component {
@@ -22,7 +21,25 @@ class Projects extends Component {
             <div className="dashboard-container">
                 <Sidebar history={this.props.history} selected="projects"/>
                 <div className="project-container">
-                    <BreadcrumbsWithIcon/>
+                <Breadcrumb className="custom-breadcrumb">
+                    <Col md={6}>
+                        <Breadcrumb.Item href="/projects">
+                            <i className="fa fa-folder breadcrumb-text" aria-hidden="true"></i>
+                            <span className="breadcrumb-text">
+                                Projects
+                            </span>
+                        </Breadcrumb.Item>
+                    </Col>
+                    <Col md={6} className="text-right">
+                        <span className="margin-l-5">
+                            1 projects
+                        </span>
+                        <i className="fa fa-plus margin-l-20" aria-hidden="true"></i>
+                        <span className="margin-l-5">
+                            Add Project
+                        </span>
+                    </Col>
+                </Breadcrumb>
                 </div>
             </div>
 		);
